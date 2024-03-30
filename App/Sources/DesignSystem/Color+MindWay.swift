80951299a9a91f6e5641d76fb14a29254dae1f4e
shwaaaa <shwaa1026@naver.com>
2024-03-30 23:37:58 +0900

    ✨ :: [#8] Color+MindWay  추가

--- App/Sources/DesignSystem/Color+MindWay.swift
+++ App/Sources/DesignSystem/Color+MindWay.swift
@@ -1,1 +1,22 @@
+import UIKit
 
+public extension UIColor {
+    enum MindWayColorSystem {
+        case black(Black)
+        case gray(Gray)
+        case main(Main)
+        case system(System)
+        case white(White)
+    }
+
+    static func mindway(_ style: MindWayColorSystem) -> UIColor {
+        switch style {
+        case let .black(colorable as MindWayColorable),
+            let .gray(colorable as MindWayColorable),
+            let .main(colorable as MindWayColorable),
+            let .system(colorable as MindWayColorable),
+            let .white(colorable as MindWayColorable):
+            return colorable.color
+        }
+    }
+}
