e3c637172fbfc862765d6c28214c9d9f98e132bb
shwaaaa <shwaa1026@naver.com>
2024-03-31 22:35:25 +0900

    ✨ :: [#8] White ColorSystem 추가

--- App/Sources/DesignSystem/Color+MindWay.swift
+++ App/Sources/DesignSystem/Color+MindWay.swift
@@ -4,18 +4,18 @@ public extension UIColor {
     enum MindWayColorSystem {
         case main(Main)
         case black(Black)
+        case white(White)
         case gray(Gray)
         case system(System)
-        case white(White)
     }
 
     static func mindway(_ style: MindWayColorSystem) -> UIColor {
         switch style {
         case let .main(colorable as MindWayColorable),
             let .black(colorable as MindWayColorable),
+            let .white(colorable as MindWayColorable),
             let .gray(colorable as MindWayColorable),
-            let .system(colorable as MindWayColorable),
-            let .white(colorable as MindWayColorable):
+            let .system(colorable as MindWayColorable):
             return colorable.color
         }
     }
