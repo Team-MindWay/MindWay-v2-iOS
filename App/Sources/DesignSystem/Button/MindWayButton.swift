c2c9a71220dc2cdc60cf761a4b9f74c0c1cda370
kimkyumbi <kimgyeombi@naver.com>
2024-04-03 15:16:50 +0900

    ✨ :: [#14] Extensions Group 추가, Extensions에 buttonWrapper 추가

---
+++ App/Sources/DesignSystem/Button/MindWayButton.swift
@@ -0,0 +1,19 @@
+//
+//  MindWayButton.swift
+//  MindWay
+//
+//  Created by Mac on 4/3/24.
+//  Copyright © 2024 team.mindway. All rights reserved.
+//
+
+import SwiftUI
+
+struct MindWayButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MindWayButton()
+}
---
+++ App/Sources/DesignSystem/Button/MindWayButtonStyle.swift
@@ -0,0 +1,19 @@
+//
+//  MindWayButtonStyle.swift
+//  MindWay
+//
+//  Created by Mac on 4/3/24.
+//  Copyright © 2024 team.mindway. All rights reserved.
+//
+
+import SwiftUI
+
+struct MindWayButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MindWayButtonStyle()
+}
