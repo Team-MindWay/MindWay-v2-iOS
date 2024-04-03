a9d0d18e17b486d8f2fa4d6c86da264b20638c38
shwaaaa <shwaa1026@naver.com>
2024-04-03 21:11:36 +0900

    ✨ :: [#22] megaphone Icon 추가

--- App/Sources/Feature/MainFeature/MainView.swift
+++ App/Sources/Feature/MainFeature/MainView.swift
@@ -1,19 +1,1 @@
-//
-//  MainView.swift
-//  MindWay
-//
-//  Created by 이승화 on 4/3/24.
-//  Copyright © 2024 team.mindway. All rights reserved.
-//
 
-import SwiftUI
-
-struct MainView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    MainView()
-}
