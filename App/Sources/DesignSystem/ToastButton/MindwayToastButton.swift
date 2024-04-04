405b73c0cb0f3b9bee03419c870212e2c91e0105
“xixn2” <ssbkh0802@naver.com>
2024-04-04 18:09:16 +0900

    💄 :: [#15] ToastButton Font 적용이동

--- App/Sources/DesignSystem/ToastButton/MindwayToastButton.swift
+++ App/Sources/DesignSystem/ToastButton/MindwayToastButton.swift
@@ -22,13 +22,14 @@ public struct MindwayToastButton: View {
                 .frame(height: 60)
                 .shadow(color: .black.opacity(0.12), radius: 10, x: 0, y: 0)
                 .padding(.horizontal, 24)
+                
             
             HStack(spacing: 0) {
                 Image(applicationStatus ? "MindWayCheckGreen" : "MindWayCheckRed")
                     .padding(.trailing, 8)
                 
                 Text(text)
-                    .font(.system(size: 16))
+                    .mindWayRegularFont(.m3)
                 
                 Spacer()
             }
