cab47335c14c63688a633aca1e81b861105debbc
kimkyumbi <kimgyeombi@naver.com>
2024-04-04 19:46:32 +0900

    🔀 :: [#23] LoginPage 퍼블리싱

---
+++ App/Sources/Feature/LoginFeature/LoginView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Image("MindWay")
+                .padding(.bottom, 334)
+            
+            Button {
+                #warning("나중에 로그인 추가하기")
+            } label: {
+                HStack(spacing: 8) {
+                    Spacer()
+                    
+                    Image("GAuthLogo")
+                    
+                    Image("SignIn")
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 10)
+                .padding(.vertical, 16)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.mindway(.main(.main)))
+                }
+            }
+            .padding(.horizontal, 24)
+        }
+    }
+}
