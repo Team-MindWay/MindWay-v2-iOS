05c3f187a7109888bda3962d02259367e76d583c
shwaaaa <shwaa1026@naver.com>
2024-04-09 15:11:21 +0900

    ♻️  :: [#32] MainPage 리팩토링

---
+++ App/Sources/Feature/MainFeature/SettingGoalReadingView.swift
@@ -0,0 +1,19 @@
+//
+//  SettingGoalReadingView.swift
+//  MindWay
+//
+//  Created by 이승화 on 4/9/24.
+//  Copyright © 2024 team.mindway. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingGoalReadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingGoalReadingView()
+}
