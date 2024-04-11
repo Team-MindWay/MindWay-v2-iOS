ba1069c3c90775c7d959d7b559ad9d09387fb4eb
kimkyumbi <kimgyeombi@naver.com>
2024-04-12 08:55:42 +0900

    🔀 :: [#38] buttonpushed -> main으로 수정

--- App/Sources/DesignSystem/Button/MindWayButtonStyle.swift
+++ App/Sources/DesignSystem/Button/MindWayButtonStyle.swift
@@ -26,7 +26,7 @@ public extension MindWayButtonStyle {
         }
             
         var backgroundColor: Color {
-            configuration.isPressed ? Color.mindway(.buttonpushed(.buttonpushed)) : Color.mindway(.main(.main))
+            configuration.isPressed ? Color.mindway(.main(.buttonpushed)) : Color.mindway(.main(.main))
         }
         
         public var body: some View {
