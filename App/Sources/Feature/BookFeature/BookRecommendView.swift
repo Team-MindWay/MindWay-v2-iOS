dbde47b837b32b111e8a1ceccc7f7b7242bf4109
kimkyumbi <kimgyeombi@naver.com>
2024-04-15 11:16:44 +0900

    🔀 :: [#33] BookFeature 퍼블리싱

--- App/Sources/Feature/BookFeature/BookRecommendView.swift
+++ App/Sources/Feature/BookFeature/BookRecommendView.swift
@@ -1,19 +1,81 @@
-//
-//  BookRecommendView.swift
-//  MindWay
-//
-//  Created by Mac on 4/12/24.
-//  Copyright © 2024 team.mindway. All rights reserved.
-//
-
 import SwiftUI
 
-struct BookRecommendView: View {
+struct RecommendBookView: View {
+    @State private var topNavigationState: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack(spacing: 20) {
+                Button {
+                    topNavigationState = false
+                } label: {
+                    VStack(spacing: 0) {
+                        Text("Novel")
+                            .foregroundColor(topNavigationState ? Color.mindway(.gray(.g4)) : Color.mindway(.black(.black)))
+                            .padding(.horizontal, 12)
+                            .padding(.vertical, 8)
+                            .mindWaySemiboldFont(.m1)
+                        
+                        if topNavigationState == false {
+                            Rectangle()
+                                .foregroundColor(.mindway(.main(.main)))
+                                .frame(width: 80, height: 5)
+                                .cornerRadius(8)
+                        }
+                    }
+                }
+                
+                Button {
+                    topNavigationState = true
+                } label: {
+                    VStack(spacing: 0) {
+                        Text("Essay")
+                            .foregroundColor(topNavigationState ? Color.mindway(.black(.black)) : Color.mindway(.gray(.g4)))
+                            .padding(.horizontal, 12)
+                            .padding(.vertical, 8)
+                            .mindWaySemiboldFont(.m1)
+                        
+                        if topNavigationState == true {
+                            Rectangle()
+                                .foregroundColor(.mindway(.main(.main)))
+                                .frame(width: 80, height: 5)
+                                .cornerRadius(8)
+                        }
+                    }
+                }
+                
+                Spacer()
+                
+                MindWayAsset.Icons.addBlack.swiftUIImage
+                    .buttonWrapper {}
+            }
+            .padding(.horizontal, 24)
+            
+            TabView(selection: $topNavigationState) {
+                ScrollView(showsIndicators: false) {
+                    VStack(spacing: 20) {
+                        ForEach(1...5, id: \.self) { _ in
+                            RecommendBook()
+                                .padding(.horizontal, 24)
+                        }
+                    }
+                }
+                .tag(false)
+                
+                ScrollView(showsIndicators: false) {
+                    VStack(spacing: 20) {
+                        ForEach(1...5, id: \.self) { _ in
+                            RecommendBook()
+                                .padding(.horizontal, 24)
+                        }
+                    }
+                }
+                .tag(true)
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .padding(.vertical, 28)
+            
+        }
+        .padding(.vertical, 20)
     }
-}
-
-#Preview {
-    BookRecommendView()
 }
