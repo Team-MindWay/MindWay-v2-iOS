55aaa72d751b3c457eaa5b3f6811361d1aa96d08
shwaaaa <shwaa1026@naver.com>
2024-04-18 20:57:29 +0900

    ♻️  :: [#47] MindWayAlertButtonType / style 추가

--- App/Sources/DesignSystem/Alert/MindWayAlertButtonType.swift
+++ App/Sources/DesignSystem/Alert/MindWayAlertButtonType.swift
@@ -3,16 +3,16 @@ import Foundation
 public struct mindWayAlertButtonType: Identifiable {
     public let id: String = UUID().uuidString
     let text: String
-//    let style: CTAButton.CTAStyle
+    let style: MindWayButton.ButtonStyleType
     let action: () -> Void
 
     public init(
         text: String = "",
-//        style: CTAButton.CTAStyle = .default,
+        style: MindWayButton.ButtonStyleType = .default,
         action: @escaping () -> Void = {}
     ) {
         self.text = text
-//        self.style = style
+        self.style = style
         self.action = action
     }
 }
