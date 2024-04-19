5ef7c95484a0942eab80f0b4b4cd16e81acb4619
shwaaaa <shwaa1026@naver.com>
2024-04-19 14:48:11 +0900

    ♻️  :: [#47] BookOrderView / MindWayAlert 적용

--- App/Sources/Feature/BookOrderFeature/BookOrderView.swift
+++ App/Sources/Feature/BookOrderFeature/BookOrderView.swift
@@ -5,8 +5,8 @@ struct BookOrderView: View {
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
-        ZStack {
-            NavigationView {
+        NavigationView {
+            ZStack {
                 VStack(spacing: 0) {
                     VStack(spacing: 28) {
                         MindWayTextField(
@@ -71,40 +71,15 @@ struct BookOrderView: View {
                     }
                 }
             }
-
-            if viewModel.isWarning == true {
-                Color.mindway(.black(.black))
-                    .ignoresSafeArea()
-                    .opacity(0.4)
-
-                warningPopup()
-                    .padding(.horizontal, 24)
-
-                #warning("Animation 추가(가능하다면)")
-            }
+            .mindWayAlert(
+                title: "신청하려는 책이 도서관에 있는지 확인해주세요. \n부적절한 도서 신청시 통보없이 삭제되며, \n후에 불이익이 있을 수 있습니다.",
+                isShowing: $viewModel.isWarning,
+                alertActions: [
+                    .init(text: "확인", style: .default) {
+                        viewModel.isWarning = false
+                    }
+                ]
+            )
         }
-    }
-
-    @ViewBuilder
-    func warningPopup() -> some View {
-        VStack(spacing: 28) {
-            Text("신청하려는 책이 도서관에 있는지 확인해주세요. \n부적절한 도서 신청시 통보없이 삭제되며, \n후에 불이익이 있을 수 있습니다.")
-                .multilineTextAlignment(.center)
-                .mindWayRegularFont(.m3)
-            
-            MindWayButton(
-                text: "확인",
-                buttonStyle: .default
-            ) {
-                viewModel.isWarning = false
-            }
-        }
-        .padding(.horizontal, 24)
-        .padding(.top, 24)
-        .padding(.bottom, 20)
-        .background(
-            Color.mindway(.white(.white))
-                .cornerRadius(8)
-        )
     }
 }
