82add7ae74a15dbb572668b3db737fc2a8324c2e
shwaaaa <shwaa1026@naver.com>
2024-04-23 11:27:08 +0900

    ✨ :: [#66] BottomSheet 추가

--- App/Sources/Feature/MyPageFeature/MyPageView.swift
+++ App/Sources/Feature/MyPageFeature/MyPageView.swift
@@ -1,42 +1,74 @@
 import SwiftUI
 
 struct MyPageView: View {
+    @State var isShowingBottomSheet = false
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("안녕하세요!")
-                        .mindWayRegularFont(.h3)
-                    
-                    HStack(spacing: 0) {
-                        Text("한재형")
-                            .mindWaySemiboldFont(.h3)
-                            .foregroundColor(.mindway(.main(.main)))
+        ZStack {
+            VStack(alignment: .leading, spacing: 0) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("안녕하세요!")
+                            .mindWayRegularFont(.h3)
                         
-                        Text("님")
-                            .mindWaySemiboldFont(.h3)
+                        HStack(spacing: 0) {
+                            Text("한재형")
+                                .mindWaySemiboldFont(.h3)
+                                .foregroundColor(.mindway(.main(.main)))
+                            
+                            Text("님")
+                                .mindWaySemiboldFont(.h3)
+                        }
                     }
+                    .padding(.top, 60)
+                    
+                    Spacer()
+                    
+                    MindWayAsset.Icons.seeMore.swiftUIImage
+                        .onTapGesture {
+                            isShowingBottomSheet = true
+                        }
                 }
-                .padding(.top, 60)
+                
+                Text("도서 신청 목록")
+                    .mindWayRegularFont(.label)
+                    .foregroundColor(.mindway(.gray(.g4)))
+                    .padding(.top, 40)
+                
+                ForEach(0..<2, id: \.self) { _ in
+                    BookOrderListRow()
+                        .padding(.top, 20)
+                }
                 
                 Spacer()
-                
-                MindWayAsset.Icons.seeMore.swiftUIImage
             }
-            
-            Text("도서 신청 목록")
-                .mindWayRegularFont(.label)
-                .foregroundColor(.mindway(.gray(.g4)))
-                .padding(.top, 40)
-            
-            ForEach(0..<2, id: \.self) { _ in
-                BookOrderListRow()
-                    .padding(.top, 20)
-            }
-            
-            Spacer()
+            .padding(.horizontal, 24)
         }
-        .padding(.horizontal, 24)
+        .mindWayBottomSheet(isShowing: $isShowingBottomSheet) {
+            VStack(alignment: .leading, spacing: 0) {
+                Button {
+
+                } label: {
+                    Text("MINDWAY 소개")
+                        .mindWayRegularFont(.m3)
+                        .foregroundColor(.mindway(.black(.black)))
+                        .padding(.top, 15)
+                        .padding(.bottom, 20)
+                }
+                
+                Divider()
+                
+                Button {
+                    
+                } label: {
+                    Text("로그아웃")
+                        .mindWayRegularFont(.m3)
+                        .foregroundColor(.mindway(.system(.system)))
+                        .padding(.top, 20)
+                        .padding(.bottom, 46)
+                }
+            }
+            .padding(.horizontal, 24)
+        }
     }
     
     @ViewBuilder
