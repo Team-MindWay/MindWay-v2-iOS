0a6665d6ae87da5728b9c93819bf1527fec077d0
kimkyumbi <kimgyeombi@naver.com>
2024-05-27 11:04:16 +0900

    ✨ :: [#78] SigninViewModel 추가

--- App/Sources/Feature/SigninFeature/SigninViewModel.swift
+++ App/Sources/Feature/SigninFeature/SigninViewModel.swift
@@ -1,9 +1,25 @@
-//
-//  SigninViewModel.swift
-//  MindWay
-//
-//  Created by Mac on 5/22/24.
-//  Copyright © 2024 team.mindway. All rights reserved.
-//
-
 import Foundation
+import Service
+
+final class SigninViewModel: ObservableObject {
+    private weak var model: (any SigninActionProtocol)?
+    private let loginUseCase: any LoginUseCase
+    
+    init(
+        model: any SigninActionProtocol,
+        loginUseCase: any LoginUseCase
+    ) {
+        self.model = model
+        self.loginUseCase = loginUseCase
+    }
+    
+    func signin(code: String) {
+        Task {
+            do {
+                let userSignupInfo = try await self.loginUseCase.execute(code: code)
+            } catch {
+                model?.updateIsError(isError: true)
+            }
+        }
+    }
+}
