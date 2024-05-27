a35e9163e862e981bbd03b46fe32d64794b9dc57
kimkyumbi <kimgyeombi@naver.com>
2024-05-27 11:01:14 +0900

    ✨ :: [#78] SigninModel 추가

--- Service/Sources/Domain/AuthDomain/Model/SigninModel.swift
+++ Service/Sources/Domain/AuthDomain/Model/SigninModel.swift
@@ -1,8 +1,16 @@
-//
-//  SigninModel.swift
-//  Service
-//
-//  Created by Mac on 5/23/24.
-//
-
 import Foundation
+
+final class SigninModel: ObservableObject, SigninStateProtocol {
+    @Published var isError: Bool = false
+    @Published var isSuccess: Bool = false
+}
+
+extension SigninModel: SigninActionProtocol {
+    func updateIsError(isError: Bool) {
+        self.isError = isError
+    }
+
+    func updateIsSuccess(isSuccess: Bool) {
+        self.isSuccess = isSuccess
+    }
+}
