f917b878e2cf288ed7eac384dd3169643d9da92f
kimkyumbi <kimgyeombi@naver.com>
2024-05-27 11:04:37 +0900

    ✨ :: [#78] SigninView 수정

--- App/Sources/Feature/SigninFeature/SigninView.swift
+++ App/Sources/Feature/SigninFeature/SigninView.swift
@@ -1,21 +1,26 @@
 import SwiftUI
+import Service
 
 struct SigninView: View {
+    @StateObject var viewModel: SigninViewModel
+    
+    init(
+        viewModel: SigninViewModel
+    ) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+    
     var body: some View {
         VStack {
             MindWayAsset.Images.mindWay.swiftUIImage
                 .padding(.bottom, 334)
             
             GAuthButtonView { code in
-                
+                viewModel.signin(code: code)
             }
-            .padding(.horizontal, 20)
+            .padding(.horizontal, 24)
             .frame(height: 50)
             .padding(.bottom, 16)
         }
     }
-}
-
-#Preview {
-    SigninView()
 }
