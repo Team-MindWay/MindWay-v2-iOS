9f1f17a3b95fd22a9b6b40bc2f51b112ff97dcd8
kimkyumbi <kimgyeombi@naver.com>
2024-05-27 11:02:12 +0900

    ✨ :: [#78] LocalAuthDataSourceImpl 추가

--- Service/Sources/Interface/DataSource/LocalAuthDataSourceImpl.swift
+++ Service/Sources/Interface/DataSource/LocalAuthDataSourceImpl.swift
@@ -1,8 +1,18 @@
-//
-//  LocalAuthDataSourceImpl.swift
-//  Service
-//
-//  Created by Mac on 5/21/24.
-//
-
 import Foundation
+
+struct LocalAuthDataSourceImpl: LocalAuthDataSource {
+    private let keyChain: Keychain
+
+    init(
+        keyChain: Keychain
+    ) {
+        self.keyChain = keyChain
+    }
+    
+    func logout() async throws {
+        keyChain.delete(type: .accessToken)
+        keyChain.delete(type: .accessExpiredAt)
+        keyChain.delete(type: .refreshToken)
+        keyChain.delete(type: .refreshExpiredAt)
+    }
+}
