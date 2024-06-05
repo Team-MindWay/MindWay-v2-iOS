b8bdfa5e6507f9ce5570dfa4165568358de874e1
kimkyumbi <kimgyeombi@naver.com>
2024-06-05 09:20:25 +0900

    ✨ :: [#78] EraseToAnyView 추가

--- App/Sources/DesignSystem/Extensions/View+eraseToAnyView.swift
+++ App/Sources/DesignSystem/Extensions/View+eraseToAnyView.swift
@@ -1,9 +1,7 @@
-//
-//  View+eraseToAnyView.swift
-//  MindWay
-//
-//  Created by Mac on 6/4/24.
-//  Copyright © 2024 team.mindway. All rights reserved.
-//
+import SwiftUI
 
-import Foundation
+public extension View {
+    func eraseToAnyView() -> AnyView {
+        AnyView(self)
+    }
+}
