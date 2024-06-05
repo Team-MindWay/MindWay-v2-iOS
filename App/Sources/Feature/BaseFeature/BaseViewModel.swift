7030fcf66c3675df6c8c024efa88261960ee051b
kimkyumbi <kimgyeombi@naver.com>
2024-06-05 09:21:51 +0900

    ✨ :: [#78] BaseViewModel 추가

--- App/Sources/Feature/BaseFeature/BaseViewModel.swift
+++ App/Sources/Feature/BaseFeature/BaseViewModel.swift
@@ -1,9 +1,31 @@
-//
-//  BaseViewModel.swift
-//  MindWay
-//
-//  Created by Mac on 6/4/24.
-//  Copyright © 2024 team.mindway. All rights reserved.
-//
+import UIKit
 
-import Foundation
+open class BaseViewModel: ObservableObject {
+    @Published public var isErrorOccurred = false
+    @Published public var isLoading = false
+    @Published public var errorMessage = ""
+
+    public init() {}
+
+    public func addCancellable<T>(
+        _ task: @escaping @Sendable () async throws -> T,
+        onReceiveValue: @escaping (T) -> Void,
+        onReceiveError: ((Error) -> Void)? = nil
+    ) {
+        isLoading = true
+        Task {
+            do {
+                let value = try await task()
+                onReceiveValue(value)
+            } catch {
+                if let onReceiveError {
+                    onReceiveError(error)
+                }
+
+                errorMessage = error.localizedDescription
+                isErrorOccurred = true
+            }
+            isLoading = false
+        }
+    }
+}
