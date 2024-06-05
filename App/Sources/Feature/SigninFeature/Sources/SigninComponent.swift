6c036c58537bfb380ac6f2fcb0644933181f23bb
kimkyumbi <kimgyeombi@naver.com>
2024-06-05 09:26:19 +0900

    ✨ :: [#78] SigninComponent 추가

--- App/Sources/Feature/SigninFeature/Sources/SigninComponent.swift
+++ App/Sources/Feature/SigninFeature/Sources/SigninComponent.swift
@@ -1,2 +1,19 @@
-import Foundation
+import NeedleFoundation
+import Service
+import SwiftUI
 
+public protocol LoginDependency: Dependency {
+    var loginUseCase: any LoginUseCase { get }
+    var signinFactory: any SigninFactory { get }
+}
+
+public final class SigninComponent: Component<LoginDependency>, SigninFactory {
+    public func makeView() -> some View {
+        SigninView(
+            viewModel: .init(
+                loginUseCase: dependency.loginUseCase
+            ),
+            signinFactory: dependency.signinFactory
+        )
+    }
+}
