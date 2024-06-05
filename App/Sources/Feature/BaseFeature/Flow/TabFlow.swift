b199d3b02331384c770d120985c7923cd0d50d15
kimkyumbi <kimgyeombi@naver.com>
2024-06-05 09:21:19 +0900

    ✨ :: [#78] TabFlow 추가

--- App/Sources/Feature/BaseFeature/Flow/TabFlow.swift
+++ App/Sources/Feature/BaseFeature/Flow/TabFlow.swift
@@ -1,9 +1,8 @@
-//
-//  TabFlow.swift
-//  MindWay
-//
-//  Created by Mac on 6/4/24.
-//  Copyright © 2024 team.mindway. All rights reserved.
-//
-
 import Foundation
+
+public enum TabFlow: String, RawRepresentable {
+    case main
+    case event
+    case book
+    case my
+}
