12280485591a5d7a95ff6aa344e4ed07fecdd7e3
kimkyumbi <kimgyeombi@naver.com>
2024-06-11 23:01:51 +0900

    ♻️ :: [#78] errorDiscription

--- Service/Sources/Domain/AuthDomain/Error/AuthDomainError.swift
+++ Service/Sources/Domain/AuthDomain/Error/AuthDomainError.swift
@@ -5,3 +5,16 @@ public enum AuthDomainError: Error {
     case notFound
     case internalServerError
 }
+
+extension AuthDomainError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .unauthorized:
+            return "권한이 없습니다."
+        case .notFound:
+            return "존재하지 않는 계정입니다."
+        case .internalServerError:
+            return "알 수 없는 에러가 발생했습니다. 지속될 시 문의 주세요."
+        }
+    }
+}
