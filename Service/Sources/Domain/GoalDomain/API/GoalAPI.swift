9a64011dbdf3611890a748efefd26b8d441b6993
shwaaaa <shwaa1026@naver.com>
2024-06-14 22:02:38 +0900

    ✨ :: [#85] SettingGoalRequestDTO 추가

---
+++ Service/Sources/Domain/GoalDomain/API/GoalAPI.swift
@@ -0,0 +1,56 @@
+import Foundation
+import Moya
+
+public enum GoalAPI {
+    case settingGoal
+    case fetchGoal
+}
+
+extension GoalAPI: MindWayAPI {
+    public typealias ErrorType = RankDomainError
+    
+    public var domain: MindWayDomain {
+        .goal
+    }
+    
+    public var urlPath: String {
+        switch self {
+        case .settingGoal, .fetchGoal:
+            return ""
+        }
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+        case .settingGoal:
+            return .post
+        case .fetchGoal:
+            return .get
+        }
+    }
+    
+    public var task: Moya.Task {
+        switch self {
+        case let .settingGoal(req):
+            return .requestJSONEncodable(req)
+        case .fetchGoal:
+            return .requestPlain
+        }
+    }
+    
+    public var jwtTokenType: JwtTokenType {
+        switch self {
+        case .rank:
+            return .accessToken
+        }
+    }
+    
+    public var errorMap: [Int: ErrorType] {
+        switch self {
+        case .rank:
+            return [
+                401: .unauthorized
+            ]
+        }
+    }
+}
