50d3af4aa02c281b0c4e4d174445edce771ce07e
kimkyumbi <kimgyeombi@naver.com>
2024-06-17 17:14:42 +0900

    ✨ :: [#87] RemoteOrderDataSource 추가

--- Service/Sources/Interface/DataSource/Order/RemoteOrderDataSource.swift
+++ Service/Sources/Interface/DataSource/Order/RemoteOrderDataSource.swift
@@ -1,8 +1,7 @@
-//
-//  RemoteOrderDataSource.swift
-//  Service
-//
-//  Created by Mac on 6/17/24.
-//
-
 import Foundation
+
+public protocol RemoteOrderDataSource {
+    func orderBook(req: OrderBookRequestDTO) async throws
+    func updateOrder(OrderID: String, req: UpdateBookRequestDTO) async throws
+    func deleteOrder(OrderID: String) async throws
+}
