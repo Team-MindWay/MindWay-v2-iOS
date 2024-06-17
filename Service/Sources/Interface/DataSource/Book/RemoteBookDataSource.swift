1c322273e8f21b0c4e7c7322f4d61bfe6a4d14cf
shwaaaa <shwaa1026@naver.com>
2024-06-17 10:14:46 +0900

    ✨ :: [#86] RemoteBookDataSource 추가

---
+++ Service/Sources/Interface/DataSource/Book/RemoteBookDataSource.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public protocol RemoteBookDataSource {
+    func writeBook(req: WriteBookRequestDTO) async throws
+    func fetchBookList() async throws -> [BookInfoEntity]
+    func modifyBook(req: BookDetialInfoEntity) async throws
+    func deleteBook() async throws
+    func fetchBookDetail() async throws -> BookDetialInfoEntity
+}
