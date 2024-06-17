cedc8b7abb69b50d650cbc1d7b302d51eb3ffb36
kimkyumbi <kimgyeombi@naver.com>
2024-06-17 17:12:31 +0900

    ✨ :: [#87] OrderBookUseCaseImpl 추가

--- Service/Sources/Domain/OrderDomain/UseCase/OrderBookUseCaseImpl.swift
+++ Service/Sources/Domain/OrderDomain/UseCase/OrderBookUseCaseImpl.swift
@@ -1,8 +1,15 @@
-//
-//  OrderBookUseCaseImpl.swift
-//  Service
-//
-//  Created by Mac on 6/17/24.
-//
-
 import Foundation
+
+public struct OrderBookUseCaseImpl: OrderBookUseCase {
+    private let orderRepository: any OrderRepository
+    
+    init(
+        orderRepository: any OrderRepository
+    ) {
+        self.orderRepository = orderRepository
+    }
+    
+    public func execute(req: OrderBookRequestDTO) async throws {
+        try await orderRepository.orderBook(req: req)
+    }
+}
