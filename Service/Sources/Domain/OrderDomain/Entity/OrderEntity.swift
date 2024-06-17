a2677204882407f73fed059b3b76a8191775a8c1
kimkyumbi <kimgyeombi@naver.com>
2024-06-17 17:13:46 +0900

    ✨ :: [#87] OrderEntity 추가

--- Service/Sources/Domain/OrderDomain/Entity/OrderEntity.swift
+++ Service/Sources/Domain/OrderDomain/Entity/OrderEntity.swift
@@ -1,8 +1,17 @@
-//
-//  OrderEntity.swift
-//  Service
-//
-//  Created by Mac on 6/17/24.
-//
-
 import Foundation
+
+public struct OrderEntity: Equatable {
+    public let title: String
+    public let author: String
+    public let bookUrl: String
+    
+    init(
+        title: String,
+        author: String,
+        bookUrl: String
+    ) {
+        self.title = title
+        self.author = author
+        self.bookUrl = bookUrl
+    }
+}
