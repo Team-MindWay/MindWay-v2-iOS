2187e213b11d9cfefc2853cc7fbc8fe8f9711c84
kimkyumbi <kimgyeombi@naver.com>
2024-06-17 17:11:25 +0900

    ✨ :: [#87] UpdateBookRequestDTO 추가

--- Service/Sources/Domain/OrderDomain/DTO/Request/UpdateBookRequestDTO.swift
+++ Service/Sources/Domain/OrderDomain/DTO/Request/UpdateBookRequestDTO.swift
@@ -1,8 +1,23 @@
-//
-//  UpdateBookRequestDTO.swift
-//  Service
-//
-//  Created by Mac on 6/17/24.
-//
-
 import Foundation
+
+public struct UpdateBookRequestDTO: Encodable {
+    public let title: String
+    public let author: String
+    public let bookUrl: String
+    
+    init(
+        title: String,
+        author: String,
+        bookUrl: String
+    ) {
+        self.title = title
+        self.author = author
+        self.bookUrl = bookUrl
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case author
+        case bookUrl = "book_url"
+    }
+}
