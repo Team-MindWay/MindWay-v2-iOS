beb4bfbd0b7e7308951cf6b50d5f046901a9b6f3
kimkyumbi <kimgyeombi@naver.com>
2024-06-17 17:12:16 +0900

    ✨ :: [#87] OrderRepositoryImpl 추가

--- Service/Sources/Domain/OrderDomain/Repository/OrderRepositoryImpl.swift
+++ Service/Sources/Domain/OrderDomain/Repository/OrderRepositoryImpl.swift
@@ -1,8 +1,23 @@
-//
-//  OrderRepositoryImpl.swift
-//  Service
-//
-//  Created by Mac on 6/17/24.
-//
-
 import Foundation
+
+public struct OrderRepositoryImpl: OrderRepository {
+    private let remoteOrderDataSource: RemoteOrderDataSource
+    
+    init(
+        remoteOrderDataSource: RemoteOrderDataSource
+    ) {
+        self.remoteOrderDataSource = remoteOrderDataSource
+    }
+    
+    public func orderBook(req: OrderBookRequestDTO) async throws {
+        try await remoteOrderDataSource.orderBook(req: req)
+    }
+    
+    public func deleteOrder(OrderID: String) async throws {
+        try await remoteOrderDataSource.deleteOrder(OrderID: OrderID)
+    }
+    
+    public func updateOrder(OrderID: String, req: UpdateBookRequestDTO) async throws {
+        try await remoteOrderDataSource.updateOrder(OrderID: OrderID, req: req)
+    }
+}
