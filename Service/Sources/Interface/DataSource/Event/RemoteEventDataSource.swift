721a0d2fc042c10ea0e26e3a2a0b4c43e1020b86
“xixn2” <ssbkh0802@naver.com>
2024-06-29 16:20:03 +0900

    🔥 :: [#92] Delete RemoteEventDataSource fetchEventDate

--- Service/Sources/Interface/DataSource/Event/RemoteEventDataSource.swift
+++ Service/Sources/Interface/DataSource/Event/RemoteEventDataSource.swift
@@ -3,5 +3,4 @@ import Foundation
 public protocol RemoteEventDataSource {
     func fetchEventInfo() async throws -> [EventInfoEntity]
     func fetchEventDetail(eventID: String) async throws -> EventDetailEntity
-    func fetchEventDate() async throws -> [EventInfoDateEntity]
 }
