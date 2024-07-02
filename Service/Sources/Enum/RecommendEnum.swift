898f91207964b1d9dd7eeafd0d4bb3bac977853d
“xixn2” <ssbkh0802@naver.com>
2024-07-02 23:24:51 +0900

    ✨ :: [#100] Add Recommend Domain RecommendEnum

---
+++ Service/Sources/Enum/RecommendEnum.swift
@@ -0,0 +1,13 @@
+//
+//  RecommendEnum.swift
+//  Service
+//
+//  Created by 서지완 on 7/2/24.
+//
+
+import Foundation
+
+public enum RecommendEnum: String, Codable, CaseIterable {
+    case novel = "NOVEL"
+    case essay = "ESSAY"
+}
